649fb5ae83b7313e39d91be9552a8639eb72f801
Francis Murtagh <francis.murtagh@arm.com>
2025-06-09 13:29:33 +0000

    CLNTFRAME-238 Get tosa2spirv working with TOSA 1.0

--- scripts/clone_tosa_serialization_library.sh
+++ scripts/clone_tosa_serialization_library.sh
@@ -11,15 +11,13 @@ TOSA_SERIALIZATION_LIB_DIR=$EXTERNAL_DIR/serialization_lib
 
 rm -rf $TOSA_SERIALIZATION_LIB_DIR
 
-TOSA_SERIALIZATION_LIB_REVISION="5a85d261cfef40ad8746eabe5f9108ea51811ed8" # v1.0 April release
+TOSA_SERIALIZATION_LIB_REVISION="d14952584ffb83372343edbada07c5ba35a4aaf6" # v1.0 Remove incompatible hardened flags
 
 echo "+++ Cloning TOSA Serialization Library"
 git clone "https://review.mlplatform.org/tosa/serialization_lib" $TOSA_SERIALIZATION_LIB_DIR
 
 pushd $TOSA_SERIALIZATION_LIB_DIR > /dev/null
   git checkout $TOSA_SERIALIZATION_LIB_REVISION
-  # Cherrypick an android fix
-  git fetch ssh://$USER@eu-gerrit-1.euhpc.arm.com:29418/mltech/mirrors/tosa/serialization_lib refs/changes/01/757901/1 && git cherry-pick FETCH_HEAD
 
   # Initialise Flatbuffers dependency, which is currently v2.0.6
   git submodule update --init --recursive
